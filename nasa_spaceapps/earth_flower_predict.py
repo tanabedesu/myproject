699cf78019c3fbe66b684b8441caa983d7b01934
tanabedesu <ti22tan7@gmail.com>
2025-10-10 13:52:06 +0900

    Update earth_flower_predict.py

--- nasa_spaceapps/earth_flower_predict.py
+++ nasa_spaceapps/earth_flower_predict.py
@@ -481,7 +481,7 @@ def country_to_iso3(name):
 df['ISO_A3'] = df['country_name'].apply(country_to_iso3)
 df_bloom = df[['date','NDVI','GDD_cumsum','country_name','ISO_A3']].copy()
 
-df_bloom_reduced = df_bloom.iloc[::10, :]
+df_bloom_reduced = df_bloom.iloc[::5, :]
 dates = df_bloom['date'].sort_values().unique()
 
 fig = go.Figure(
@@ -565,6 +565,7 @@ if st.button("Show GDD Animation"):
         ax.set_title(f"Cumulative GDD on {date.date()} (Japan)", fontsize=16)
 
         placeholder.pyplot(fig)
+
 
 
 
